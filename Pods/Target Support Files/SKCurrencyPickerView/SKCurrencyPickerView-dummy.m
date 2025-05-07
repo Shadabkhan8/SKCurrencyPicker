42a044a0145dfaedb14011007d7407f28f63ec9d
Shadabkhan8 <shadab.khan@infoenum.com>
2025-05-07 14:57:53 +0530

    First Commit

---
+++ Pods/Target Support Files/SKCurrencyPickerView/SKCurrencyPickerView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SKCurrencyPickerView : NSObject
+@end
+@implementation PodsDummy_SKCurrencyPickerView
+@end
---
+++ SKCurrencyPicker/PickerOrientation.swift
@@ -0,0 +1,12 @@
+//
+//  PickerOrientation.swift
+//  RBCountryPickerView
+//
+//  Created by IE14 on 10/04/25.
+//
+
+public enum PickerOrientation {
+    case vertical
+    case horizontal
+
+}
